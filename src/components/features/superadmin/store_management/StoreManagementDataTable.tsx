fa41a42e256f7fdf630369df4d7c781788266246
souvick.jash <souvick.jash@weavers-web.com>
2026-03-23 11:04:20 +0530

    fix

--- src/components/features/superadmin/store_management/StoreManagementDataTable.tsx
+++ src/components/features/superadmin/store_management/StoreManagementDataTable.tsx
@@ -9,6 +9,7 @@ import { useFetchAllStores } from '@/hooks/storeManagementHooks';
 const StoreManagementDataTable = () => {
   const { data, isError, error } = useFetchAllStores();
   console.log('data+++++', data);
+  console.log('error+++++', error);
   const content = data ?? [];
 
   useEffect(() => {
